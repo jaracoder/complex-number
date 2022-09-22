1d20274ef689c185cde3c9cb31aed356620e318f
jripolldev <juan.ripoll.93@gmail.com>
2022-09-22 20:45:54 +0200

    Publica la clase ComplexNumber con la representación de un número complejo.

---
+++ ComplexNumber.vb
@@ -0,0 +1,64 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class ComplexNumber
+
+    Dim _real As Double
+    Dim _imaginario As Double
+
+    Public Sub New()
+        _real = 0
+        _imaginario = 0
+    End Sub
+
+    Public Sub Dispose()
+        _real = 0
+        _imaginario = 0
+    End Sub
+
+    Public Sub Establecer(ByVal real As Double, ByVal imaginario As Double)
+        _real = real
+        _imaginario = imaginario
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return CStr(_real) & "+" & CStr(_imaginario) & "i"
+    End Function
+
+    Public Function Suma(ByVal Sumando As ComplexNumber) As ComplexNumber
+        Dim _ComplejoAux As ComplexNumber = New ComplexNumber()
+
+        _ComplejoAux._real = Me._real + Sumando._real
+        _ComplejoAux._imaginario = Me._imaginario + Sumando._imaginario
+
+        Return _ComplejoAux
+    End Function
+
+    Public Function Resta(ByVal sustraendo As ComplexNumber) As ComplexNumber
+        Dim _ComplejoAux As ComplexNumber = New ComplexNumber()
+
+        _ComplejoAux._real = Me._real - sustraendo._real
+        _ComplejoAux._imaginario = Me._imaginario - sustraendo._imaginario
+
+        Return _ComplejoAux
+    End Function
+
+    Public Function Multiplicacion(ByVal Multiplicando As ComplexNumber) As ComplexNumber
+        Dim _ComplejoAux As ComplexNumber = New ComplexNumber()
+        '(A*C-B*D)
+        _ComplejoAux._real = (Me._real * Multiplicando._real) - (Me._imaginario * Multiplicando._imaginario)
+        '(A*D+B*C)i
+        _ComplejoAux._imaginario = (Me._real * Multiplicando._imaginario) + (Me._imaginario * Multiplicando._real)
+        Return _ComplejoAux
+    End Function
+
+    Public Function Multiplicacion(ByVal NumeroReal As Double) As ComplexNumber
+        Dim _ComplejoAux As ComplexNumber = New ComplexNumber()
+        '(xA,xBi)
+        _ComplejoAux._real = NumeroReal * Me._real
+        _ComplejoAux._imaginario = NumeroReal * Me._imaginario
+
+        Return _ComplejoAux
+    End Function
+
+End Class
---
+++ ComplexNumberTest.vb
@@ -0,0 +1,27 @@
+﻿Module ComplexNumberTest
+
+    Sub Main()
+        Dim number1 As New ComplexNumber()
+        Dim number2 As New ComplexNumber()
+
+        number1.Establecer(13.333, 11.003457)
+        Console.WriteLine("Num 1: " & number1.ToString())
+
+        number2.Establecer(9.0001, 10.123)
+        Console.WriteLine("Num 2: " & number2.ToString())
+
+        Console.WriteLine()
+
+        Dim suma As ComplexNumber = number1.Suma(number2)
+        Console.WriteLine("Suma: " & suma.ToString())
+
+        Dim resta As ComplexNumber = number1.Resta(number2)
+        Console.WriteLine("Resta: " & suma.ToString())
+
+        Dim multi As ComplexNumber = number1.Multiplicacion(number2)
+        Console.WriteLine("Multiplicacion: " & suma.ToString())
+
+        Console.Read()
+    End Sub
+
+End Module
